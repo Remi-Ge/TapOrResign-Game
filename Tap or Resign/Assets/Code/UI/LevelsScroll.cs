ee0bc06db98881f6f3bb341f5f0d063514206605
Remi-Dv <remi.gentils@gmail.com>
2023-10-15 22:40:53 +0200

    levels scroll optimization

--- Tap or Resign/Assets/Code/UI/LevelsScroll.cs
+++ Tap or Resign/Assets/Code/UI/LevelsScroll.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Code.UI
@@ -8,17 +9,17 @@ namespace Code.UI
     {
         [SerializeField] private RectTransform scrollRectContent;
         [SerializeField] private GameObject levelPrefab;
-        private int _levelNumber;
+        private int _levelsNumber;
         private readonly List<Tuple<int, RectTransform>> _levels = new List<Tuple<int, RectTransform>>(); //the spawned levels
 
         private void Start()
         {
-            SetScrollAreaSize(10);
+            SetScrollAreaSize(50);
         }
 
         private void SetScrollAreaSize(int levelNumber)
         {
-            _levelNumber = levelNumber;
+            _levelsNumber = levelNumber;
             scrollRectContent.offsetMin = new Vector2(scrollRectContent.offsetMin.x,
                 -500 * (levelNumber - 1));
 
@@ -33,24 +34,44 @@ namespace Code.UI
             GameObject newLevel = Instantiate(levelPrefab, scrollRectContent);
             RectTransform levelRectTransform = newLevel.GetComponent<RectTransform>(); 
             levelRectTransform.anchoredPosition = 
-                new Vector2(0, 500 * (_levelNumber - 1) / 2f + -500 * levelIndex);
+                new Vector2(0, 500 * (_levelsNumber - 1) / 2f + -500 * levelIndex);
             _levels.Add(new Tuple<int, RectTransform>(levelIndex, levelRectTransform));
         }
 
         public void ScrollRectMoved()
         {
+            //max distance from the middle of the screen
+            float maxLevelsDistance = 3000f;
+            
             float contentTop = scrollRectContent.offsetMax.y;
+            
             //checks if the levels are too far
             for (int i = 0; i < _levels.Count; i++)
             {
-                if (Mathf.Abs(contentTop - 500 * _levels[i].Item1) > 3000)
+                if (Mathf.Abs(contentTop - 500 * _levels[i].Item1) > maxLevelsDistance)
                 {
                     Destroy(_levels[i].Item2.gameObject);
                     _levels.RemoveAt(i);
                     i--;
                 }
             }
-            //rect top value x -1 to have the position of the screen
+            //checks the closest levels
+            //the level below the screen
+            int highestLevelIndex = _levels.Max(tuple => tuple.Item1);
+            //Debug.Log(highestLevelIndex+1 + " " + Mathf.Abs(contentTop - (highestLevelIndex + 1) * 500));
+            if (Mathf.Abs(contentTop - (highestLevelIndex + 1) * 500) < maxLevelsDistance
+                && highestLevelIndex + 1 < _levelsNumber)
+            {
+                SpawnLevel(highestLevelIndex + 1);
+            }
+            //the level above the screen
+            int lowestLevelIndex = _levels.Min(tuple => tuple.Item1);
+            //Debug.Log(lowestLevelIndex-1 + " " + Mathf.Abs(contentTop - (lowestLevelIndex + 1) * 500));
+            if (Mathf.Abs(contentTop - (lowestLevelIndex - 1) * 500) < maxLevelsDistance
+                && lowestLevelIndex - 1 >= 0 && lowestLevelIndex < _levelsNumber)
+            {
+                SpawnLevel(lowestLevelIndex - 1);
+            }
         }
     }
 }
