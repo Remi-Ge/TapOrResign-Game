d4b8764b39e5ac4379d7fdc6595b6bac5f44727b
Remi-Dv <remi.gentils@gmail.com>
2023-10-15 15:54:52 +0200

    added structs

--- Tap or Resign/Assets/Code/SavesData/DataStructs/LevelStruct.cs
+++ Tap or Resign/Assets/Code/SavesData/DataStructs/LevelStruct.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace Code.SavesData.DataStructs
 {
+    [Serializable]
     public struct LevelStruct
     {
+        public string levelName;
+        public byte difficulty;
         
     }
 }
---
+++ Tap or Resign/Assets/Code/SavesData/DataStructs/LogoStruct.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Code.SavesData.DataStructs
+{
+    [Serializable]
+    public struct LogoStruct
+    {
+        public int background;
+        public int ground;
+        public int groundItem;
+        public int flyingItem;
+    }
+}
